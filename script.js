2c16ea228ef39c2e117b5d3b9e4e3c0609a147aa
ashhadimam07 <ashhadimam07@gmail.com>
2021-02-24 22:32:44 +0530

    Update value

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const rupee = document.querySelector('.rs');
 const dollar = document.querySelector('.dollar');
-
+const indianVal = document.querySelector(".value");
 
 function rsToDollar() {
 fetch('https://api.exchangeratesapi.io/latest?base=USD&symbols=INR')
 .then(response => response.json())
 .then(json => {
-    dollar.value = ((rupee.value / json.rates.INR) * 10).toFixed(2);
+  indianVal.innerHTML = Number(json.rates.INR).toFixed(2);
+  dollar.value = ((rupee.value / json.rates.INR) * 10).toFixed(2);
 });
 }
 
@@ -18,8 +19,19 @@ function dollarToRs() {
     fetch('https://api.exchangeratesapi.io/latest?base=USD&symbols=INR')
     .then(response => response.json())
     .then(json => {
+      indianVal.innerHTML = Number(json.rates.INR).toFixed(2);
       rupee.value = ((dollar.value * json.rates.INR) / 10).toFixed(2);
     });
 }
 
 dollar.addEventListener('input', dollarToRs);
+
+function updateValue() {
+fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=INR`)
+    .then(response => response.json())
+    .then(json => {
+      indianVal.innerHTML = Number(json.rates.INR).toFixed(2);
+    });
+}
+
+updateValue();
